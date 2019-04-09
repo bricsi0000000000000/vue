9c2263436b900400f512d22c4651045175f3039e
Bólya Richárd <44650681+bricsi0000000000000@users.noreply.github.com>
2019-04-09 21:17:53 +0200

    viz graph szöveg kijelzése

--- RecipeBuilder/RecipeBuilder.js
+++ RecipeBuilder/RecipeBuilder.js
@@ -21,6 +21,33 @@ var vm = new Vue({
       { task: 'C1', eq: 'E3', proctime: 3 },
       { task: 'C2', eq: 'E1', proctime: 6 },
       { task: 'C2', eq: 'E2', proctime: 4 },
-    ]
+    ],
+    vizGraphSzoveg:""
+  },
+  methods:{
+    vizGraphOut(){
+      this.vizGraphSzoveg += "digraph SGraph { rankdir=LR node [shape=circle,fixedsize=true,width=0.9,label=<<B>\\N</B>>]";
+      for(i=0; i< this.precedences.length; i++){
+        this.vizGraphSzoveg += " " + this.precedences[i].task1 + " [ label = < <B>\\N</B><BR/>{";
+        for(j=0; j< this.proctimes.length; j++){
+          if(this.proctimes[j].task === this.precedences[i].task1){
+            this.vizGraphSzoveg += this.proctimes[j].eq + ",";
+          }
+        }
+        this.vizGraphSzoveg = this.vizGraphSzoveg.substring(0,this.vizGraphSzoveg.length-1);
+        this.vizGraphSzoveg +="}> ]";
+      }
+
+      for(i=0; i< this.precedences.length; i++){
+        this.vizGraphSzoveg += " " + this.precedences[i].task1 + " -> " + this.precedences[i].task2;
+        for(j=0; j< this.proctimes.length; j++){
+          if(this.proctimes[j].task === this.precedences[i].task1){
+            this.vizGraphSzoveg += " [ label = " + this.proctimes[j].proctime + " ]";
+          }
+        }
+      }
+
+      this.vizGraphSzoveg += "}";
+    }
   }
 })
