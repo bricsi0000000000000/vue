22caf7e7bffdff2890d7adb256893337083163cf
Bólya Richárd <44650681+bricsi0000000000000@users.noreply.github.com>
2019-04-26 11:02:58 +0200

    Graph nélküli, csak graph szöveggel

--- RecipeBuilder/RecipeBuilder.js
+++ RecipeBuilder/RecipeBuilder.js
@@ -1,53 +1,200 @@
 var vm = new Vue({
   el: '#RecipeBuilder',
-  data: {
-    precedences: [
-      { task1: 'A1', task2: 'A2' },
-      { task1: 'A2', task2: 'A' },
-      { task1: 'B1', task2: 'B2' },
-      { task1: 'B2', task2: 'B3' },
-      { task1: 'B3', task2: 'B' },
-      { task1: 'C1', task2: 'C2' },
-      { task1: 'C2', task2: 'C' },
-    ],
-    proctimes: [
-      { task: 'A1', eq: 'E4', proctime: 4 },
-      { task: 'A2', eq: 'E3', proctime: 5 },
-      { task: 'B1', eq: 'E2', proctime: 7 },
-      { task: 'B2', eq: 'E1', proctime: 3 },
-      { task: 'B2', eq: 'E2', proctime: 5 },
-      { task: 'B3', eq: 'E3', proctime: 6 },
-      { task: 'C1', eq: 'E4', proctime: 4 },
-      { task: 'C1', eq: 'E3', proctime: 3 },
-      { task: 'C2', eq: 'E1', proctime: 6 },
-      { task: 'C2', eq: 'E2', proctime: 4 },
-    ],
-    vizGraphSzoveg:""
-  },
-  methods:{
-    vizGraphOut(){
-      this.vizGraphSzoveg += "digraph SGraph { rankdir=LR node [shape=circle,fixedsize=true,width=0.9,label=<<B>\\N</B>>]";
-      for(i=0; i< this.precedences.length; i++){
-        this.vizGraphSzoveg += " " + this.precedences[i].task1 + " [ label = < <B>\\N</B><BR/>{";
-        for(j=0; j< this.proctimes.length; j++){
-          if(this.proctimes[j].task === this.precedences[i].task1){
-            this.vizGraphSzoveg += this.proctimes[j].eq + ",";
-          }
-        }
-        this.vizGraphSzoveg = this.vizGraphSzoveg.substring(0,this.vizGraphSzoveg.length-1);
-        this.vizGraphSzoveg +="}> ]";
-      }
+  data() {
+    return{
+      taskName: '',
+      productName: '',
+      tasks: [], //name, product
 
-      for(i=0; i< this.precedences.length; i++){
-        this.vizGraphSzoveg += " " + this.precedences[i].task1 + " -> " + this.precedences[i].task2;
-        for(j=0; j< this.proctimes.length; j++){
-          if(this.proctimes[j].task === this.precedences[i].task1){
-            this.vizGraphSzoveg += " [ label = " + this.proctimes[j].proctime + " ]";
-          }
-        }
-      }
+      equipmentName:'',
+      equipments: [],
 
-      this.vizGraphSzoveg += "}";
+      warningTxt:"",
+
+      task1:"",
+      task2:"",
+      precedences: [], //task1, task2
+
+      tasksAndProducts:[],
+
+      proctimes:[], //task, eq, proctime
+      proctime:"",
+      proctime_task:"",
+      proctime_eq:"",
+
+      taskEquipment:[], //which task which equipments | task, eqs []
+
+      vizGraphTxt:"",
     }
-  }
+  }, 
+  methods:{
+    addTask(){
+        this.tasks.push({name: this.taskName, product: this.productName});
+        this.addTasksAndProducts();
+        this.taskName='';
+        this.productName='';
+        this.deleteEmptyTask();
+    },
+    deleteTask(id){
+      this.tasks.splice(id,1);
+    },
+    addTasksAndProducts(){
+      this.tasksAndProducts.push(this.taskName);
+
+      yes=true;
+      for(i=0; i< this.tasksAndProducts.length; i++){
+        if(this.tasksAndProducts[i] === this.productName){
+          yes=false;
+        }
+      }
+      if(yes){
+        this.tasksAndProducts.push(this.productName);
+      }
+    },
+    addEquipment(){
+      this.equipments.push(this.equipmentName);
+      this.equipmentName='';
+
+      this.deleteEmptyEq();
+    },
+    deleteEq(id){
+      this.equipments.splice(id,1);
+    },
+    addPrecedence(){
+      this.precedences.push({task1: this.task1, task2: this.task2});
+
+      this.task1="";
+      this.task2="";
+
+      this.deleteSamePrecedences();
+      this.deleteDuplicatePrecedence();
+    },
+    deletePrecendence(id){
+      this.precedences.splice(id,1);
+    },
+    addProctime(){
+      this.proctimes.push({task: this.proctime_task, eq: this.proctime_eq, proctime: this.proctime});
+      this.proctime="";
+      this.proctime_task="",
+      this.proctime_eq="",
+
+      this.deleteEmptyProctime();
+    },
+    deleteProctime(id){
+      this.proctimes.splice(id,1);
+    },
+    deleteEmptyProctime(){
+      for(i=0; i< this.proctimes.length; i++){
+        if(this.proctimes[i].task === ""){
+          this.deleteProctime(i);
+          this.showWarning("PROCTIME: task name is empty");
+        }
+        else if(this.proctimes[i].eq === ""){
+          this.deleteProctime(i);
+          this.showWarning("PROCTIME: equipment name is empty");
+        }
+        else if(this.proctimes[i].proctime === ""){
+          this.deleteProctime(i);
+          this.showWarning("PROCTIME: proctime name is empty");
+        }
+        else{
+          this.warningTxt="";
+        }
+      }
+    },
+    deleteEmptyTask(){
+      for(i=0; i< this.tasks.length; i++){
+        if(this.tasks[i].name === ''){
+          this.deleteTask(i);
+          this.showWarning("TASKS: task name is empty");
+        }
+        else if(this.tasks[i].product === ''){
+          this.deleteTask(i);
+          this.showWarning("TASKS: product name is empty");
+        }
+        else{
+          this.warningTxt="";
+        }
+      }
+    },
+    deleteEmptyEq(){
+      for(i=0; i< this.equipments.length; i++){
+        if(this.equipments[i] === ''){
+          this.deleteEq(i);
+          this.showWarning("EQUIPMENTS: Equipment name is empty");
+        }
+        else{
+          this.warningTxt="";
+        }
+      }
+    },
+    showWarning(text){
+      this.warningTxt = text;
+    },
+    deleteDuplicatePrecedence(){
+      for(i=0; i < this.precedences.length-1; i++){
+        if(this.precedences[i].task1 === this.precedences[i+1].task1 &&
+          this.precedences[i].task2 === this.precedences[i+1].task2){
+          this.deletePrecendence(i,1);
+          this.showWarning("Same precedences");
+          }
+      }
+    },
+    deleteSamePrecedences(){
+      for(i=0; i< this.precedences.length; i++){
+        if(this.precedences[i].task1 === this.precedences[i].task2){
+          this.deletePrecendence(i,1);
+          this.showWarning("Same tasks");
+        }
+      }
+    },
+    equipmentsToTask(){
+      for(i=0; i< this.proctimes.length; i++){
+        taskTemp = this.proctimes[i].task;
+        eqTemp=[];
+        for(j=0; j< this.proctimes.length; j++){
+          if(this.proctimes[j].task === taskTemp){
+            eqTemp.push(this.proctimes[j].eq);
+          }
+        }
+       
+        yes=true;
+        for(j=0; j< this.taskEquipment.length; j++){
+          if(this.taskEquipment[j].task === taskTemp){
+            yes=false;
+          }
+        }
+        if(yes){
+          this.taskEquipment.push({task: taskTemp, eqs: eqTemp});
+        }
+      }
+    },
+    vizGraphTxtOut(){
+      this.equipmentsToTask();
+      this.vizGraphTxt ="digraph SGraph { rankdir=LR 	node [shape=circle,fixedsize=true,width=0.9,label=<<B>\\N</B>>]"
+      for(i=0; i< this.taskEquipment.length; i++){
+        this.vizGraphTxt += " " + this.taskEquipment[i].task + " [ " + "label = < <B>\\N</B><BR/>{";
+        for(j=0; j< this.taskEquipment[i].eqs.length; j++){
+          this.vizGraphTxt += this.taskEquipment[i].eqs[j] +",";
+        }
+        this.vizGraphTxt = this.vizGraphTxt.substring(0,this.vizGraphTxt.length-1);
+        this.vizGraphTxt += "}> ]";
+      }
+
+      for(i=0; i< this.precedences.length; i++){
+        this.vizGraphTxt += this.precedences[i].task1 + " -> " + this.precedences[i].task2;
+
+        minProctime = this.proctimes[0].proctime;
+        for(j=0; j< this.proctimes.length; j++){
+          if(this.proctimes[j].task === this.precedences[i].task1){
+            if(this.proctimes[j].proctime < minProctime){
+              minProctime = this.proctimes[j].proctime;
+            }
+          }
+        }
+        this.vizGraphTxt += " [ label = " + minProctime + " ]";
+      }
+      this.vizGraphTxt += "}";
+    },
+  },
 })
